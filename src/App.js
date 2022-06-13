36d7724091104fe82d56ad940477d9cfe73150ce
Mourya Prasanth <mouryaprasanthp@gmail.com>
2022-06-13 08:46:07 +0530

    Add components for ToDo App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import TodoList from "./components/TodoList";
+import Layout from './components/Layout';
+import tasksData from './data/tasks.json';
+import './App.css'
 
 function App() {
+  const [tasks, setTasks] = useState(tasksData);
+  const defaultTask = {
+    title: "",
+    id: null,
+    description: "",
+    deadline: null,
+    tags: [],
+    priority: false,
+    image: "",
+    completed: false,
+    createdAt: null
+  };
+
+  const onCreate = newTask => {
+    newTask.title.length > 0 && setTasks([newTask, ...tasks]);
+  };
+
+  const onUpdate = (task, id) => {
+    let tempList = tasks;
+    let index = tasks.findIndex(t => t.id === id);
+    tempList[index] = task;
+    setTasks([...tempList]);
+  };
+  const onDelete = (id) => {
+    let tempList = tasks.filter(task => task.id !== id);
+    setTasks(tempList);
+  };
+
+  useEffect(() => {
+    let arr = localStorage.getItem("tasks");
+    if (arr) {
+      setTasks(JSON.parse(arr));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (tasks && tasks.length > 0) {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  }, [tasks]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <TodoList tasks={tasks} onCreate={onCreate} onUpdate={onUpdate} onDelete={onDelete} defaultTask={defaultTask} />
+    </Layout>
   );
 }
 
